9d35e0dac2f677fab66ae6e09df8ca55226d833e
ucla99 <hirosait@gmail.com>
2018-05-24 13:29:04 +0900

    add again

--- linear_optimization.py
+++ linear_optimization.py
@@ -1,70 +1,42 @@
 ###
-# 線形最適化　練習問題 利益最大化
+# 01ナップサック問題
 #
-# 豚丼、鶏丼、ミックス丼の３種類がある。
-# 豚丼を作るには、200gの豚肉と100gの鶏肉
-# 鶏丼を作るには、100gの豚丼と200gの鶏肉
-# ミックス丼は豚、鶏、牛をそれぞれ100g
-# 豚と鶏はそれぞれ6kg, 牛は3kgしかない
-# 価格は豚丼1500円、鶏丼1800円、ミックス丼3000円
-# 利益を最大化するには、丼を何杯ずつ作ればよいのだろうか？
-#
+# ナップサックは最大7kgまで入れられる
+# ぬいぐるみは４種類ある
+# 極小 16万円 2kg
+# 小   19万円 3kg
+# 中   23万円 4kg
+# 大   28万円 5kg
+# なるべく価値の高いぬいぐるみを持ち出すには、ナップサックにどれを入れるか
 #
 # 定式化
 #
-# 変数：豚丼 x1, 鶏丼 x2, ミックス丼 x3
-# 目的関数：15x1 + 18x2 + 30x3
-# 制約=肉量の上限 (非負制約）
-# 　豚：2x1 + x2 + x3 <= 60
-# 　鶏：x1 + 2x2 + x3 <= 60
-# 　牛：x3 <= 30
-##
-
-from ortools.linear_solver import pywraplp
-
+# 持っていくときは１，持っていかないときは０ -> バイナリ変数
+# 目的関数：16x1 + 19x2 + 23x3 + 28x4 （最大化）
+# 制約条件：2x1 + 3x2 + 4x3 + 5x4 <= 7
+# 変数：x1, x2, x3, x4 ∈ {0,1}
+#
+#
+from ortools.algorithms import pywrapknapsack_solver
 
 def main():
+    solver = pywrapknapsack_solver.KnapsackSolver(
+        pywrapknapsack_solver.KnapsackSolver.KNAPSACK_DYNAMIC_PROGRAMMING_SOLVER, 'test'
+    )
 
-    solver = pywraplp.Solver('線形最適化問題', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+    values = [16,19,23,28]
+    weights = [[2,3,4,5]]
+    capacity = [7]
+    solver.Init(values, weights, capacity)
+    computed_value = solver.Solve()
 
-    # 変数を定義
-    x1 = solver.NumVar(-solver.infinity(), solver.infinity(), 'x1')
-    x2 = solver.NumVar(-solver.infinity(), solver.infinity(), 'x2')
-    x3 = solver.NumVar(-solver.infinity(), solver.infinity(), 'x3')
+    packed_items = [x for x in range(0, len(weights[0]))
+                    if solver.BestSolutionContains(x)]
+    packed_weights = [weights[0][i] for i in packed_items]
 
-    # 制約を定義
-    constraint1 = solver.Constraint(-solver.infinity(), 60)
-    constraint1.SetCoefficient(x1, 2)
-    constraint1.SetCoefficient(x2, 1)
-    constraint1.SetCoefficient(x3, 1)
-
-    constraint2 = solver.Constraint(-solver.infinity(), 60)
-    constraint2.SetCoefficient(x1, 1)
-    constraint2.SetCoefficient(x2, 2)
-    constraint2.SetCoefficient(x3, 1)
-
-    constraint3 = solver.Constraint(-solver.infinity(), 30)
-    constraint3.SetCoefficient(x3, 1)
-
-    # 目的関数の設定
-    objective = solver.Objective()
-    objective.SetCoefficient(x1, 15)
-    objective.SetCoefficient(x2, 18)
-    objective.SetCoefficient(x3, 30)
-
-    objective.SetMaximization()
-
-    solver.Solve()
-
-    opt_solution = 15 * x1.solution_value() + 18 * x2.solution_value() + 30 * x3.solution_value()
-    print('変数の数：{}'.format(solver.NumVariables()))
-    print('制約の数：{}'.format(solver.NumConstraints()))
-    print('==最適解==')
-    print('x1 = ', x1.solution_value())
-    print('x2 = ', x2.solution_value())
-    print('x3 = ', x3.solution_value())
-    print('目的関数値 =', opt_solution)
-
+    print("持ち出しアイテム：", packed_items)
+    print("持ち出すアイテムの重さ： ", packed_weights)
+    print("持ち出しアイテムの合計の価値：", computed_value)
 
 if __name__ == '__main__':
     main()
